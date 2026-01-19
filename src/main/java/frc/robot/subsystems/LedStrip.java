2c0e527dabf8e53764acc6aaadc8072789fdf446
Mateo Eerlingen <mail@wurdle.eu>
2026-01-19 01:15:49 +0000

    fix density, rename constants in LedStrip

--- src/main/java/frc/robot/subsystems/LedStrip.java
+++ src/main/java/frc/robot/subsystems/LedStrip.java
@@ -10,22 +10,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LedStrip extends SubsystemBase {
 
-    public static final int ledPort = 9;
-    public static final int ledCount = 30;
+    public static final int LED_PORT = 9;
+    public static final int LED_COUNT = 30;
+    public static final double LED_DENSITY_LEDS_PER_METER = 1.0 / 60.0;
 
     private final AddressableLED m_led;
     private final AddressableLEDBuffer m_buffer;
     private final LEDPattern m_rainbowPattern;
 
     public LedStrip() {
-        m_led = new AddressableLED(ledPort);
-        m_buffer = new AddressableLEDBuffer(ledCount);
+        m_led = new AddressableLED(LED_PORT);
+        m_buffer = new AddressableLEDBuffer(LED_COUNT);
 
-        m_led.setLength(ledCount);
+        m_led.setLength(LED_COUNT);
 
         m_led.start();
 
-        m_rainbowPattern = LEDPattern.rainbow(255, 255).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), Meters.of(1.0 / ledCount));
+        m_rainbowPattern = LEDPattern.rainbow(255, 255).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), Meters.of( LED_DENSITY_LEDS_PER_METER ));
 
     }
     
