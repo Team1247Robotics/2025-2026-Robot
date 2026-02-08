91cea45f0175bea09cbd8e636e1554b54bc316cd
1247-Robotics <1247robotics@gmail.com>
2026-02-07 20:36:12 -0500

    rename that to make more sense

---
+++ src/main/java/frc/robot/commands/shooter/ShooterDependantSequence.java
@@ -0,0 +1,20 @@
+package frc.robot.commands.shooter;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterDependantSequence extends SequentialCommandGroup {
+  public ShooterDependantSequence(Shooter shooter, DoubleSupplier speed, Command... commands) {
+    super(
+      new SpinUpShooter(shooter, speed),
+      Commands.parallel(
+        new HoldShooterSpeed(shooter, speed),
+        Commands.sequence(commands)
+      )
+    );
+  }
+}
