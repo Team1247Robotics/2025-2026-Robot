c381018ed1278d0ada164043d99dccdbcbf318e1
Mr. A <GabrielArtaud@comcast.net>
2026-02-13 16:51:08 -0500

    Updated packages/paths

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,7 @@ import frc.robot.subsystems.AutoBuilder2;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LedStrip;
 import frc.robot.subsystems.motors.LonelyTalonFx;
-import frc.robot.subsystems.sensors.ColorSensor;
+import frc.robot.sensors.ColorSensor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
