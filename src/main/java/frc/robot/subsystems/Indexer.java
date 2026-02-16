099366f51cebe31bfd7ad04e3c62d0fcda3171ee
1247-Robotics <1247robotics@gmail.com>
2026-02-16 16:08:53 -0500

    generic spark max motor

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,42 +1,13 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.PersistMode;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs.IndexerMotor;
 import frc.robot.Constants.IndexerConstants;
+import frc.robot.subsystems.generics.GenericSparkMaxMotor;
 
-public class Indexer extends SubsystemBase {
-  private final SparkMax m_motor = new SparkMax(IndexerConstants.kMotorCanId, MotorType.kBrushless);
-  private final RelativeEncoder m_encoder;
-  private final SparkClosedLoopController m_clController;
-
+public class Indexer extends GenericSparkMaxMotor {
   public Indexer() {
-    m_encoder = m_motor.getEncoder();
-    m_clController = m_motor.getClosedLoopController();
-    m_motor.configure(IndexerMotor.config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
-
-  public double getPosition() {
-    return m_encoder.getPosition();
-  }
-
-  public double getVelocity() {
-    return m_encoder.getVelocity();
-  }
-
-  public void setPosition(double target) {
-    m_clController.setSetpoint(target, ControlType.kPosition, ClosedLoopSlot.kSlot0);
-  }
-
-  public void setVelocity(double target) {
-    m_clController.setSetpoint(target, ControlType.kVelocity, ClosedLoopSlot.kSlot1);
+    super(IndexerConstants.kMotorCanId, MotorType.kBrushless, IndexerMotor.config);
   }
 }
