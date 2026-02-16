9f8a5e4ec17603c6e14104bf8f48c3bf63fef23f
Noah <noah.clark@kearsarge.org>
2026-02-15 22:13:57 -0500

    BROKEN, KARL FIX!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import frc.robot.subsystems.LonelyTalonFx;
 import frc.robot.sensors.ColorSensor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -94,7 +95,11 @@ public class RobotContainer {
                 false),
             m_robotDrive));
 
-    m_ledStrip.setDefaultCommand(new LedStripScrollRainbow(m_ledStrip).ignoringDisable(true));
+    m_ledStrip.setDefaultCommand(
+        ConditionalCommand(
+          new LedStripScrollRainbow(m_ledStrip).ignoringDisable(true),
+          new LedStripSetAlianceColor(m_ledStrip).ignoringDisable(true),
+          () -> {return DriverStation.isDisabled();}));
 
 
     /* 
