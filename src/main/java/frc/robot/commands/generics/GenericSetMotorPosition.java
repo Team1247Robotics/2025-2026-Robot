39f8bee20c344e2d4c07f6dcca7e139a9d35167e
1247-Robotics <1247robotics@gmail.com>
2026-02-19 15:48:24 -0500

    generic set motor position

---
+++ src/main/java/frc/robot/commands/generics/GenericSetMotorPosition.java
@@ -0,0 +1,56 @@
+package frc.robot.commands.generics;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.Generics;
+import frc.robot.subsystems.generics.GenericSparkMaxMotor;
+
+public class GenericSetMotorPosition extends Command {
+  private final DoubleSupplier m_targetSupplier;
+  protected final GenericSparkMaxMotor m_motor;
+  protected final double m_allowableError = Generics.MotorPositionControlAllowableError;
+  private double m_target = 0;
+  
+  public GenericSetMotorPosition(GenericSparkMaxMotor motor, DoubleSupplier targetSupplier) {
+    m_targetSupplier = targetSupplier;
+    m_motor = motor;
+    addRequirements(m_motor);
+  }
+  
+  public GenericSetMotorPosition(GenericSparkMaxMotor motor, double targetPosition) {
+    this(motor, () -> targetPosition);
+  }
+
+  protected double getDistanceFromTarget(double target) {
+    double currentPos = m_motor.getPosition();
+    return Math.abs(currentPos - target);
+  }
+
+  protected double getDistanceFromTarget() {
+    return getDistanceFromTarget(m_target);
+  }
+
+  protected void moveToTarget(double target) {
+    m_motor.setPosition(target);
+  }
+
+  protected void moveToTarget() {
+    moveToTarget(m_target);
+  }
+
+  @Override
+  public void initialize() {
+    m_target = m_targetSupplier.getAsDouble();
+  }
+
+  @Override
+  public void execute() {
+    moveToTarget();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return getDistanceFromTarget() < m_allowableError;
+  }
+}
--- src/main/java/frc/robot/commands/indexer/SetIndexerPosition.java
+++ src/main/java/frc/robot/commands/indexer/SetIndexerPosition.java
@@ -2,55 +2,16 @@ package frc.robot.commands.indexer;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.IndexerConstants;
+import frc.robot.commands.generics.GenericSetMotorPosition;
 import frc.robot.subsystems.Indexer;
 
-public class SetIndexerPosition extends Command {
-  private final DoubleSupplier m_targetSupplier;
-  protected final Indexer m_indexer;
-  protected final double m_allowableError = IndexerConstants.Control.allowableError;
-  private double m_target = 0;
+public class SetIndexerPosition extends GenericSetMotorPosition {
   
   public SetIndexerPosition(Indexer indexer, DoubleSupplier targetSupplier) {
-    m_targetSupplier = targetSupplier;
-    m_indexer = indexer;
-    addRequirements(indexer);
+    super(indexer, targetSupplier);
   }
   
   public SetIndexerPosition(Indexer indexer, double targetPosition) {
-    this(indexer, () -> targetPosition);
-  }
-
-  protected double getDistanceFromTarget(double target) {
-    double currentPos = m_indexer.getPosition();
-    return Math.abs(currentPos - target);
-  }
-
-  protected double getDistanceFromTarget() {
-    return getDistanceFromTarget(m_target);
-  }
-
-  protected void moveToTarget(double target) {
-    m_indexer.setPosition(target);
-  }
-
-  protected void moveToTarget() {
-    moveToTarget(m_target);
-  }
-
-  @Override
-  public void initialize() {
-    m_target = m_targetSupplier.getAsDouble();
-  }
-
-  @Override
-  public void execute() {
-    moveToTarget();
-  }
-
-  @Override
-  public boolean isFinished() {
-    return getDistanceFromTarget() < m_allowableError;
+    super(indexer, () -> targetPosition);
   }
 }
